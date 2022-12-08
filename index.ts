5a37f51544532330fe5a755936b0dde8ac191ef3
Trung Duong <trungdt.cmaxx@gmail.com>
2022-12-08 13:31:54 +0700

    build api product

--- index.ts
+++ index.ts
@@ -1,42 +1,191 @@
 import { AppDataSource } from "./src/data-source";
-import { PhoneBook } from "./src/entity/PhoneBook";
+import { Product } from "./src/entity/Product";
 import multer from 'multer';
 const upload = multer();
 import express from "express";
 import bodyParser from 'body-parser';
 
-const PORT = 3000;
+const PORT = 8000;
 
 AppDataSource.initialize().then(async connection => {
   const app = express();
-  app.set("view engine", "ejs");
-  app.set("views", "./src/views");
+
   app.use(bodyParser.json());
-  app.use(express.json());
-  const PhoneBookRepo = connection.getRepository(PhoneBook);
-  app.get("/phone/create", (req, res) => {
-    res.render("create");
+
+  const ProductRepo = connection.getRepository(Product);
+
+  app.post("/product/create", async (req, res) => {
+
+    try {
+
+      const productSearch = await ProductRepo.findOneBy({ name: req.body.name });
+
+      if (productSearch) {
+
+        res.status(500).json({
+
+          mesage: "Sản phẩm đã tồn tại"
+
+        })
+
+      }
+
+      const productData = {
+
+        name: req.body.name,
+
+        avatar: req.body.avatar,
+
+        author: req.body.author,
+
+        price: req.body.price
+
+      };
+
+      const product = await ProductRepo.save(productData);
+
+      if (product) {
+
+        res.status(200).json({
+
+          message: "Create product success",
+
+          product: product
+
+        });
+
+      }
+
+    } catch (err) {
+
+      res.status(500).json({
+
+        message: err.message
+
+      })
+
+    }
+
   });
 
-  app.post("/phone/create", upload.none(), async (req, res) => {
-    const phoneData = {
-      name: req.body.name,
-      address: req.body.address,
-      email: req.body.email,
-      phone: req.body.phone
-    };
+  app.patch("/product/update", async (req, res) => {
 
-    const phone = await PhoneBookRepo.save(phoneData);
-    res.render("success");
+    try {
+
+      let productSearch = await ProductRepo.findOneBy({ id: req.body.id });
+
+      if (!productSearch) {
+
+        return res.status(500).json({
+
+          mesage: "Sản phẩm không tồn tại"
+
+        })
+
+      }
+
+      const product = await ProductRepo.update({ id: req.body.id }, req.body);
+
+      res.status(200).json({
+
+        message: "Update product success",
+
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+
+        message: err.message
+
+      })
+
+    }
+
   });
 
-  app.get("/phone/list", async (req, res) => {
-    const phoneBooks = await PhoneBookRepo.find();
-    res.render("list", { phoneBooks: phoneBooks })
+  app.delete("/product/delete", async (req, res) => {
+
+    try {
+
+      let productSearch = await ProductRepo.findOneBy({ id: req.body.id });
+
+      if (!productSearch) {
+
+        return res.status(500).json({
+
+          mesage: "Sản phẩm không tồn tại"
+
+        })
+
+      }
+
+      const product = await ProductRepo.delete({ id: req.body.id });
+
+      res.status(200).json({
+
+        message: "Delete product success",
+
+      });
+
+    } catch (err) {
+
+      res.status(500).json({
+
+        message: err.message
+
+      })
+
+    }
+
   });
 
+  app.get("/product/list", async (req, res) => {
+
+    try {
+
+      const products = await ProductRepo.find();
+
+      if (products) {
+
+        res.status(200).json({ message: "Sucess", products: products })
+
+      }
+
+    } catch (err) {
+
+      res.status(500).json({ message: err.mesage })
+
+    }
+
+  });
+
+  app.get("/product/detail", async (req, res) => {
+
+    try {
+
+      let productId = parseInt(req.query.productId as string);
+
+      const product = await ProductRepo.findOneBy({ id: productId })
+
+      if (product) {
+
+        res.status(200).json({ message: "Sucess", product })
+
+      }
+
+    } catch (err) {
+
+      res.status(500).json({ message: err.mesage })
+
+    }
+
+  });
   app.listen(PORT, () => {
+
     console.log("App running with port: " + PORT)
+
   })
+
 });
 
